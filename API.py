c35fb69aae236d370399af003479614b5f931702
NagaTheDon <nagaarjun09@gmail.com>
2018-05-08 04:56:33 +0100

    Added the Error Handling! Tested OK. DOne!

--- API.py
+++ API.py
@@ -7,11 +7,11 @@ page_names  = ["Ironman", "Thor", "Captain_America", "Black_Widow", "Hulk"]
 site = pywikibot.Site("en", "wikipedia")
 
 def update_revisions(name_of_article):
-	page = pywikibot.Page(site, "User:Nagathedon/sandbox/"+name_of_article)
-	revs = page.revisions(content=False)
-	revs = list(revs)
+    page = pywikibot.Page(site, "User:Nagathedon/sandbox/"+name_of_article)
+    revs = page.revisions(content=False)
+    revs = list(revs)
 
-	return revs
+    return revs
 
 
 app = Flask(__name__)
@@ -20,61 +20,78 @@ app = Flask(__name__)
 @app.route("/documents/", methods = ["GET"])
 
 def index():
-	return jsonify(page_names)
+    return jsonify(page_names)
 
 
 @app.route("/documents/<name>", methods = ["GET", "POST"])
 @app.route("/documents/<name>/", methods = ["GET"])
 
 def revisions(name):
-	revs = update_revisions(name)
-	timestamps = []
-	if (request.method == 'POST'):
-		some_json = request.get_json()
-		content_str = some_json.get("content")
-		text = pywikibot.Page(pywikibot.Site(), 'User:Nagathedon/sandbox/'+name).getOldVersion(oldid=revs[0]["revid"])
-		page = pywikibot.Page(site, 'User:Nagathedon/sandbox/'+name)
-		page.text = page.text.replace(text, content_str)
-		page.save('Replaced '+name+" Page") 
-		return jsonify(some_json.get("content"))
+    try:
+        revs = update_revisions(name)
+        timestamps = []
+        if (request.method == 'POST'):
+            some_json = request.get_json()
+            content_str = some_json.get("content")
+            text = pywikibot.Page(pywikibot.Site(), 'User:Nagathedon/sandbox/'+name).getOldVersion(oldid=revs[0]["revid"])
+            page = pywikibot.Page(site, 'User:Nagathedon/sandbox/'+name)
+            page.text = page.text.replace(text, content_str)
+            page.save('Replaced '+name+" Page") 
+            return jsonify(some_json.get("content"))
 
-	else:
-		for i in range(len(revs)):
-			timestamps.append(revs[i]['timestamp'])
-		return jsonify(timestamps)
+        else:
+            for i in range(len(revs)):
+                timestamps.append(revs[i]['timestamp'])
+            return jsonify(timestamps)
+
+    except pywikibot.exceptions.NoPage:
+        print("This page does not EXIST!")
+        return jsonify("This PAGE does not exist! Select one of the given pages!")
 
 
 @app.route("/documents/<num>/latest")
 @app.route("/documents/<num>/latest/")
 
 def latest(num):
-		revs = update_revisions(num)
-		text = pywikibot.Page(pywikibot.Site(), 'User:Nagathedon/sandbox/'+num).getOldVersion(oldid=revs[0]["revid"]) 
-		return jsonify(text)
+    try:
+        revs = update_revisions(num)
+        text = pywikibot.Page(pywikibot.Site(), 'User:Nagathedon/sandbox/'+num).getOldVersion(oldid=revs[0]["revid"]) 
+        return jsonify(text)
+
+    except pywikibot.exceptions.NoPage:
+        print("This page does not EXIST!")
+        return jsonify("This PAGE does not exist! Select one of the given pages!")
 
 
-@app.route("/documents/<num>/<time>")
-@app.route("/documents/<num>/<time>/")
+@app.route("/documents/<num>/<time>",methods = ["GET"])
+@app.route("/documents/<num>/<time>/",methods = ["GET"]    )
 def revision_at_timestamp(num, time):
-	revs = update_revisions(num)
-	date = datetime.strptime(time, '%a, %d %b %Y %H:%M:%S GMT')
-	received_time = str(date.isoformat())+"Z"
+    try:
+        revs = update_revisions(num)
 
-	revision_id = 0
-	for i in range(len(revs)):
-		if(received_time == str(revs[i]['timestamp'])):
-			revision_id = revs[i]["revid"]
-			break
+        date = datetime.strptime(time, '%a, %d %b %Y %H:%M:%S GMT')
+        received_time = str(date.isoformat())+"Z"
 
-	if(revision_id == 0):
-		print("ERROR: No revision with that timestamp")
-	else:
-		text = pywikibot.Page(pywikibot.Site(), 'User:Nagathedon/sandbox/'+num).getOldVersion(oldid=revision_id) 
-		return jsonify(text)
+        revision_id = 0
+        for i in range(len(revs)):
+            if(received_time == str(revs[i]['timestamp'])):
+                revision_id = revs[i]["revid"]
+                break
+
+        try:
+            text = pywikibot.Page(pywikibot.Site(), 'User:Nagathedon/sandbox/'+num).getOldVersion(oldid=revision_id) 
+            return jsonify(text)
+
+        except KeyError:
+            return jsonify("This revision does not exist! Make a revision using the POST function described in README.txt")
 
 
-	
+    except (pywikibot.exceptions.NoPage,ValueError) as e :
+        print("This page does not EXIST!")
+        return jsonify("This PAGE does not exist! Select one of the given pages! Check the format for Dates")
+
+    
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
